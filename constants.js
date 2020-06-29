c1cae6a833d7fb285bc6e374a421c176332a65f2
Alexander6RI <61212053+Alexander6RI@users.noreply.github.com>
2020-06-29 15:04:53 -0400

    Update constants.js
    added examples

--- constants.js
+++ constants.js
@@ -32,3 +32,518 @@ const NOUN_WORDS = [
 	"stibnite",
 	"didapper"
 ]
+
+
+const EXAMPLE_1 = {
+  "items": [
+    {
+      "content": "guide",
+      "type": "object",
+      "variant": "noun",
+      "connected": [
+        [
+          {
+            "content": "The",
+            "type": "modifier",
+            "variant": "adjective",
+            "connected": [
+              [],
+              []
+            ]
+          }
+        ]
+      ]
+    },
+    {
+      "content": "pointed",
+      "type": "object",
+      "variant": "verb",
+      "connected": [
+        [
+          {
+            "content": "to",
+            "type": "modifier",
+            "variant": "adjective",
+            "connected": [
+              [
+                {
+                  "content": "picture",
+                  "type": "object",
+                  "variant": "noun",
+                  "connected": [
+                    [
+                      {
+                        "content": "the",
+                        "type": "modifier",
+                        "variant": "adjective",
+                        "connected": [
+                          [],
+                          []
+                        ]
+                      }
+                    ]
+                  ]
+                }
+              ],
+              []
+            ]
+          }
+        ]
+      ]
+    }
+  ],
+  "versionString": "1.0.1"
+}
+
+const EXAMPLE_2 = {
+  "items": [
+    {
+      "content": "",
+      "type": "object_clause",
+      "variant": "noun_clause",
+      "connected": [
+        [
+          {
+            "content": "Whoever",
+            "type": "object",
+            "variant": "noun",
+            "connected": [
+              []
+            ]
+          },
+          {
+            "content": "wins",
+            "type": "object",
+            "variant": "verb",
+            "connected": [
+              []
+            ]
+          },
+          {
+            "content": "contest",
+            "type": "object",
+            "variant": "direct_object",
+            "connected": [
+              [
+                {
+                  "content": "the",
+                  "type": "modifier",
+                  "variant": "adjective",
+                  "connected": [
+                    [],
+                    []
+                  ]
+                }
+              ]
+            ]
+          }
+        ]
+      ]
+    },
+    {
+      "content": "is",
+      "type": "object",
+      "variant": "verb",
+      "connected": [
+        []
+      ]
+    },
+    {
+      "content": "friend",
+      "type": "object",
+      "variant": "predicate",
+      "connected": [
+        [
+          {
+            "content": "my",
+            "type": "modifier",
+            "variant": "adjective",
+            "connected": [
+              [],
+              []
+            ]
+          }
+        ]
+      ]
+    }
+  ],
+  "versionString": "1.0.1"
+}
+
+const EXAMPLE_3 = {
+  "items": [
+    {
+      "content": "One",
+      "type": "object",
+      "variant": "noun",
+      "connected": [
+        [
+          {
+            "content": "of",
+            "type": "modifier",
+            "variant": "adjective",
+            "connected": [
+              [
+                {
+                  "content": "points",
+                  "type": "object",
+                  "variant": "noun",
+                  "connected": [
+                    [
+                      {
+                        "content": "the",
+                        "type": "modifier",
+                        "variant": "adjective",
+                        "connected": [
+                          [],
+                          []
+                        ]
+                      },
+                      {
+                        "content": "major",
+                        "type": "modifier",
+                        "variant": "adjective",
+                        "connected": [
+                          [],
+                          []
+                        ]
+                      },
+                      {
+                        "content": "selling",
+                        "type": "modifier",
+                        "variant": "adjective",
+                        "connected": [
+                          [],
+                          []
+                        ]
+                      },
+                      {
+                        "content": "of",
+                        "type": "modifier",
+                        "variant": "adjective",
+                        "connected": [
+                          [
+                            {
+                              "content": "book (The Hitchhiker's Guide to the Galaxy)",
+                              "type": "object",
+                              "variant": "noun",
+                              "connected": [
+                                [
+                                  {
+                                    "content": "that",
+                                    "type": "modifier",
+                                    "variant": "adjective",
+                                    "connected": [
+                                      [],
+                                      []
+                                    ]
+                                  },
+                                  {
+                                    "content": "travel",
+                                    "type": "modifier",
+                                    "variant": "adjective",
+                                    "connected": [
+                                      [],
+                                      []
+                                    ]
+                                  },
+                                  {
+                                    "content": "remarkable",
+                                    "type": "modifier",
+                                    "variant": "adjective",
+                                    "connected": [
+                                      [],
+                                      [
+                                        {
+                                          "content": "wholly",
+                                          "type": "meta",
+                                          "variant": "adverb",
+                                          "connected": []
+                                        }
+                                      ]
+                                    ]
+                                  }
+                                ]
+                              ]
+                            }
+                          ],
+                          []
+                        ]
+                      }
+                    ]
+                  ]
+                }
+              ],
+              []
+            ]
+          },
+          {
+            "content": "apart from",
+            "type": "modifier",
+            "variant": "adjective",
+            "connected": [
+              [
+                {
+                  "content": "and",
+                  "type": "conjunction",
+                  "variant": "conjunction_left",
+                  "connected": [
+                    [
+                      {
+                        "content": "cheapness",
+                        "type": "object",
+                        "variant": "noun",
+                        "connected": [
+                          [
+                            {
+                              "content": "its",
+                              "type": "modifier",
+                              "variant": "adjective",
+                              "connected": [
+                                [],
+                                []
+                              ]
+                            },
+                            {
+                              "content": "relative",
+                              "type": "modifier",
+                              "variant": "adjective",
+                              "connected": [
+                                [],
+                                []
+                              ]
+                            }
+                          ]
+                        ]
+                      }
+                    ],
+                    [
+                      {
+                        "content": "fact",
+                        "type": "object",
+                        "variant": "noun",
+                        "connected": [
+                          [
+                            {
+                              "content": "the",
+                              "type": "modifier",
+                              "variant": "adjective",
+                              "connected": [
+                                [],
+                                []
+                              ]
+                            },
+                            {
+                              "content": "that",
+                              "type": "modifier",
+                              "variant": "adjective",
+                              "connected": [
+                                [
+                                  {
+                                    "content": "",
+                                    "type": "object_clause",
+                                    "variant": "noun_clause",
+                                    "connected": [
+                                      [
+                                        {
+                                          "content": "it",
+                                          "type": "object",
+                                          "variant": "noun",
+                                          "connected": [
+                                            []
+                                          ]
+                                        },
+                                        {
+                                          "content": "has",
+                                          "type": "object",
+                                          "variant": "verb",
+                                          "connected": [
+                                            []
+                                          ]
+                                        },
+                                        {
+                                          "content": "",
+                                          "type": "object_clause",
+                                          "variant": "direct_object_clause",
+                                          "connected": [
+                                            [
+                                              {
+                                                "content": "words (\"Don't Panic\")",
+                                                "type": "object",
+                                                "variant": "noun",
+                                                "connected": [
+                                                  [
+                                                    {
+                                                      "content": "the",
+                                                      "type": "modifier",
+                                                      "variant": "adjective",
+                                                      "connected": [
+                                                        [],
+                                                        []
+                                                      ]
+                                                    }
+                                                  ]
+                                                ]
+                                              },
+                                              {
+                                                "content": "written",
+                                                "type": "object",
+                                                "variant": "verb",
+                                                "connected": [
+                                                  [
+                                                    {
+                                                      "content": "in",
+                                                      "type": "modifier",
+                                                      "variant": "adjective",
+                                                      "connected": [
+                                                        [
+                                                          {
+                                                            "content": "letters",
+                                                            "type": "object",
+                                                            "variant": "noun",
+                                                            "connected": [
+                                                              [
+                                                                {
+                                                                  "content": "large",
+                                                                  "type": "modifier",
+                                                                  "variant": "adjective",
+                                                                  "connected": [
+                                                                    [],
+                                                                    []
+                                                                  ]
+                                                                },
+                                                                {
+                                                                  "content": "friendly",
+                                                                  "type": "modifier",
+                                                                  "variant": "adjective",
+                                                                  "connected": [
+                                                                    [],
+                                                                    []
+                                                                  ]
+                                                                }
+                                                              ]
+                                                            ]
+                                                          }
+                                                        ],
+                                                        []
+                                                      ]
+                                                    },
+                                                    {
+                                                      "content": "on",
+                                                      "type": "modifier",
+                                                      "variant": "adjective",
+                                                      "connected": [
+                                                        [
+                                                          {
+                                                            "content": "cover",
+                                                            "type": "object",
+                                                            "variant": "noun",
+                                                            "connected": [
+                                                              [
+                                                                {
+                                                                  "content": "its",
+                                                                  "type": "modifier",
+                                                                  "variant": "adjective",
+                                                                  "connected": [
+                                                                    [],
+                                                                    []
+                                                                  ]
+                                                                }
+                                                              ]
+                                                            ]
+                                                          }
+                                                        ],
+                                                        []
+                                                      ]
+                                                    }
+                                                  ]
+                                                ]
+                                              }
+                                            ]
+                                          ]
+                                        }
+                                      ]
+                                    ]
+                                  }
+                                ],
+                                []
+                              ]
+                            }
+                          ]
+                        ]
+                      }
+                    ]
+                  ]
+                }
+              ],
+              []
+            ]
+          }
+        ]
+      ]
+    },
+    {
+      "content": "is",
+      "type": "object",
+      "variant": "verb",
+      "connected": [
+        []
+      ]
+    },
+    {
+      "content": "glossary",
+      "type": "object",
+      "variant": "predicate",
+      "connected": [
+        [
+          {
+            "content": "its",
+            "type": "modifier",
+            "variant": "adjective",
+            "connected": [
+              [],
+              []
+            ]
+          },
+          {
+            "content": "and",
+            "type": "compound_mod",
+            "variant": "compound_adj",
+            "connected": [
+              [
+                {
+                  "content": "compendious",
+                  "type": "modifier",
+                  "variant": "adjective",
+                  "connected": [
+                    [],
+                    []
+                  ]
+                }
+              ],
+              [
+                {
+                  "content": "accurate",
+                  "type": "modifier",
+                  "variant": "adjective",
+                  "connected": [
+                    [],
+                    [
+                      {
+                        "content": "occasionally",
+                        "type": "meta",
+                        "variant": "adverb",
+                        "connected": []
+                      }
+                    ]
+                  ]
+                }
+              ]
+            ]
+          }
+        ]
+      ]
+    }
+  ],
+  "versionString": "1.0.1"
+}
